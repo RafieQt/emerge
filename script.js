2c8cbfeb826458502a4f63f204f4242e949e4111
RafieQt <tajuddinrafi76@gmail.com>
2025-08-31 11:35:10 +0600

    The coin system is done

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const heartButtons = document.getElementsByClassName("heart-btn");
         for(hearts of heartButtons){
             hearts.addEventListener("click", function(e){
             e.preventDefault();
-            coins= parseInt(document.getElementById("heart-balance").textContent);
-            document.getElementById("heart-balance").textContent = coins+1;})
+            heart= parseInt(document.getElementById("heart-balance").textContent);
+            document.getElementById("heart-balance").textContent = heart+1;})
         }
 
+//Coin system 
+const callButtons = document.getElementsByClassName("call-btn");
+
+        for (let calls of callButtons) {
+            calls.addEventListener("click", function (e) {
+                e.preventDefault();
+                let coin = parseInt(document.getElementById("coin-balance").textContent);
+                if(coin>0){
+                    document.getElementById("coin-balance").textContent = coin - 20;
+                }else{
+                    alert("Coin amount is not sufficient!");
+                }
+            });
+        }
